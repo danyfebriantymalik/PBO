35b1d564a971b20142b0ad764c608bc59552dee0
danyfebriantymalik <adry.indah@gmail.com>
2015-05-02 14:30:48 +0800

    dany

---
+++ src/dany/input.java
@@ -0,0 +1,18 @@
+package dany;
+
+public class input {
+	static String nama = "Dany Febrianty Malik";/* deklarasi variabel, string disini sebagai variabel global 
+	yang dapat digunakan disemua class*/
+	int a=3;
+	int b=5;
+	public static void main(String args[])
+	{
+		System.out.println(nama); 
+		System.out.println("Nim 60200112079");
+		System.out.println("Kelas A");
+		System.out.println("Jurusan Teknik Informatika");
+		System.out.println("Asal Parepare");
+		System.out.println("Lahir di Parepare, 07 Februari 1995");
+	
+	}
+}
